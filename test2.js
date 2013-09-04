bdd2957b52e3e680bab23a2b07e3dcb91abd7572
Krilnon <krilnon@gmail.com>
2013-09-04 17:42:39 -0400

    Add a more realistic test program.

---
+++ test2.js
@@ -0,0 +1,33 @@
+// techcash, Citi virtual credit card
+
+var
+	techcash = require('techcash'),
+    citiAuth = require('citi-auth'),
+    virtualCC = require('citi-virtual-card-api')
+
+var
+	transactions = []
+
+main()
+
+function main(){
+	refreshTechCashInfo()
+    
+    // setInterval(refreshTechCashInfo, 60 * 60 * 1000)
+}
+
+function refreshTechCashInfo(){
+  var loginInfo = techcash.loginInfo({ user: 'tim', pass: 'beaver' })
+  
+  // login server is determined by username
+  console.log('logging into techcash at address: ', loginInfo.server)
+  
+  techcash.getTransactions(loginInfo, 100, function(e){
+   	
+  })
+  
+}
+
+
+
+
