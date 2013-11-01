9010a2e95d28e237787880dba622ed730adc70c2
Krilnon <krilnon@gmail.com>
2013-11-01 17:42:37 -0400

    Make test file 3 a more useful test of jQuery and callback types.

--- test3.js
+++ test3.js
@@ -1,1 +1,10 @@
-var x = 4; // hello
+var todo = require('todo')
+var todolist = require('todolist')
+
+todo.on('serverUpdate', function(data){
+  todolist.add(data.newTodos)// {}
+})
+
+$('#remove').click(function(e){
+  todolist.removeAt(todolist.size - 1)
+})
